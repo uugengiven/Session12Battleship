8c09ce1bf1e4622a5bc8bad7aec338146492c6e9
John Lange <lange.john.m@gmail.com>
2021-03-02 11:00:12 -0500

    Initial Commit

---
+++ Session12Battleship/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Session12Battleship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
