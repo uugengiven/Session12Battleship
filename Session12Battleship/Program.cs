5886b968fcb5ee21ae2502eff900e4e295f68d9d
John Lange <lange.john.m@gmail.com>
2021-03-02 14:08:52 -0500

    Game ends and we now use strings for the playfield

--- Session12Battleship/Program.cs
+++ Session12Battleship/Program.cs
@@ -16,7 +16,9 @@ namespace Session12Battleship
             // [See] if the `cell` on the `playfield` at `userRow, userColumn` was a "hit"
             // If so, that ship is gone/sunk (say "Hit!")
             // If not, it says "miss!"
-            // [Do it all again] from `show the playfield`
+            // [See] if there are any ships left
+            // If so, [Do it all again] from `show the playfield`
+            // If not, Stop looping and show "you won!"
 
             // things still to do
             // Make it so it ends?
@@ -29,19 +31,24 @@ namespace Session12Battleship
 
             // define playfield
             // int[,] playfield = new int[8, 8];
-            int[,] playfield;
-            playfield = new int[8, 8];
+            string[,] playfield;
+            playfield = new string[8, 8];
 
             // playfield defaults to all 0s
             // 0 will be my open water
             // 1 will be my miss
             // 2 will be my hit ship
             // 3 will be my hidden ship
+            string water = null;
+            string miss = "Q";
+            string hitShip = "blazzle";
+            string hiddenShip = "shhh, don't look here";
+
 
             // add ships
-            playfield[2, 3] = 3;
-
-            while (true)
+            playfield[2, 3] = hiddenShip;
+            bool keepLooping = true;
+            while (keepLooping == true)
             {
                 // show the playfield
                 // If I see a 0, put a ?
@@ -53,17 +60,18 @@ namespace Session12Battleship
                     for (int col = 0; col < 8; col++)
                     {
                         // show the user-y version of the current cell
-                        if (playfield[row, col] == 0 || playfield[row, col] == 3)
+                        string currentCell = playfield[row, col];
+                        if (currentCell == water || currentCell == hiddenShip)
                         {
                             Console.Write("?");
                         }
 
-                        if (playfield[row, col] == 1)
+                        if (currentCell == miss)
                         {
                             Console.Write(".");
                         }
 
-                        if (playfield[row, col] == 2)
+                        if (currentCell == hitShip)
                         {
                             Console.Write("*");
                         }
@@ -81,16 +89,41 @@ namespace Session12Battleship
                 int userColumn = Convert.ToInt32(Console.ReadLine());
 
                 // [See] if the `cell` on the `playfield` at `userRow, userColumn` was a "hit"
-                if (playfield[userRow, userColumn] == 3)
+                if (playfield[userRow, userColumn] == hiddenShip)
                 {
                     // say hit then change/take away the ship
                     Console.WriteLine("Hit!");
-                    playfield[userRow, userColumn] = 2;
+                    playfield[userRow, userColumn] = hitShip;
                 }
                 else
                 {
                     Console.WriteLine("Miss!");
-                    playfield[userRow, userColumn] = 1;
+                    playfield[userRow, userColumn] = miss;
+                }
+
+                // [See] if any ships are left
+                int howManyShips = 0;
+                for (int row = 0; row < 8; row++)
+                {
+                    for (int col = 0; col < 8; col++)
+                    {
+                        if(playfield[row, col] == hiddenShip)
+                        {
+                            // I found a ship!
+                            howManyShips += 1;
+                        }
+                    }
+                }
+                if(howManyShips > 0)
+                {
+                    // there are ships
+                    Console.WriteLine("You have ships left to shoot!");
+                }
+                else
+                {
+                    // there are no ships left
+                    keepLooping = false;
+                    Console.WriteLine("You got them all, good job!");
                 }
 
             }
